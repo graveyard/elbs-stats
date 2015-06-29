eb964b3fb575043b1a5d51efa5602dc687ca5ed7
Daniel Fuentes <daniel.fuentes@clever.com>
2015-06-29 12:30:19 -0700

    initial commit

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+mkdir -p /etc/diamond
+sed -e "s/<INFLUXDB_USER>/$INFLUXDB_USER/" \
+    -e "s/<INFLUXDB_PASS>/$INFLUXDB_PASS/" \
+    -e "s/<INFLUXDB_DB>/$INFLUXDB_DB/" \
+    -e "s/<INFLUXDB_HOST>/$INFLUXDB_HOST/" \
+    -e "s/<INFLUXDB_PORT>/$INFLUXDB_PORT/" \
+    -e "s/<AWS_ACCESS_KEY_ID>/$AWS_ACCESS_KEY_ID/" \
+    -e "s/<AWS_SECRET_ACCESS_KEY>/$AWS_SECRET_ACCESS_KEY/" \
+    /usr/bin/diamond.conf.template \
+    > /etc/diamond/diamond.conf
+diamond -f --skip-change-user --skip-fork
